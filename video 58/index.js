1936f2be00f86db9f1baa787d90a578e737de18e
Arpit Mahoviya <mahoviyaarpit@gmail.com>
2025-12-24 22:02:17 +0530

    JS Functions

---
+++ video 58/index.js
@@ -0,0 +1,22 @@
+function nice(name){
+    console.log("hello " +name+ " what are you doing")
+    console.log("hello " +name+ " what are you eating")
+}
+nice("Arpit")
+console.log("Hey Anhad what are you doing")
+console.log("Hey Anhad what are you eating")
+
+function sum(a,b){
+    //console.log(a+b)
+    return a+b
+}
+result=sum(5,10)
+
+console.log("The sum of 5 and 10 is:", result)
+
+const func1=(x)=>{
+    console.log("I am a funciton and my name is ", x)
+}
+func1(43);
+func2(66);
+func3(88);
